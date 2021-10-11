1837552edc61878552551f63fff226497e3fdfac
Your Name <flaittj@gmail.com>
2021-10-11 01:25:13 -0300

    add env variables options

--- back/nodejs-api/src/data-access/index.js
+++ back/nodejs-api/src/data-access/index.js
@@ -1,5 +1,7 @@
 import makeDeviceManagerDb from "./devices-db";
 
+const urlDb = process.env['URL_DB'] || "mysql://username:password@localhost:3306/devices_manager";
+
 export async function makeDb(){
     if(global.connection && global.connection.state !== 'disconnected'){
         console.log("Conexão já existente")
@@ -12,7 +14,8 @@ export async function makeDb(){
     }
 
     const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection("mysql://username:password@database-1.cc14cf7siydh.us-east-2.rds.amazonaws.com:3306/devices_manager");
+    // console.log("urldb:", urlDb)
+    const connection = await mysql.createConnection(urlDb);
     // const connection = await mysql.createConnection("mysql://root:MySql2019!@localhost:3306/devices_manager");
     console.log("Conectou no MySQL!");
     global.connection = connection;
--- frontend/src/app/components/categories/category.service.ts
+++ frontend/src/app/components/categories/category.service.ts
@@ -2,13 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Category } from './category.model';
+import { environment } from 'src/environments/environment';'../../../environments/environment';
+
+const baseUrl = environment.URL_BACKEND || 'http://localhost:3003';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  baseUrl = 'http://localhost:3000';
   options = 
       {
         'Content-Type':'application/json',
@@ -19,17 +21,17 @@ export class CategoryService {
   constructor(private http: HttpClient) { }
 
   create<T>(category: Category) : Observable<T> {
-    return this.http.post<T>(`${this.baseUrl}/category`, {
+    return this.http.post<T>(`${baseUrl}/category`, {
       name: category.name,
     }, {headers: this.options})
   }
 
   read(): Observable<Category[]> {
-    return this.http.get<Category[]>(`${this.baseUrl}/categories`, {headers: this.options})
+    return this.http.get<Category[]>(`${baseUrl}/categories`, {headers: this.options})
   }
 
   delete<T>(category: Category) : Observable<T> {
-    return this.http.delete<T>(`${this.baseUrl}/category`, {
+    return this.http.delete<T>(`${baseUrl}/category`, {
       headers: this.options,
       body: {
         id: category.id
